2813a7dcf79ab3f61e37ff455552c0d31844d111
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-02 23:38:10 +0300

    venus: remove nfc

--- device.mk
+++ device.mk
@@ -13,11 +13,3 @@ $(call inherit-product-if-exists, vendor/xiaomi/venus/venus-vendor.mk)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
-
-# NFC
-PRODUCT_PACKAGES += \
-    NfcNci \
-    Tag
-
-PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service
